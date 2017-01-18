7e8ba9133e4de6e04a3cf428a75a37016dfa1f87
Hanna Johansson <hanna93.hej@gmail.com>
2017-01-18 13:43:39 +0100

    updated comments

--- functions.js
+++ functions.js
@@ -1,1 +1,14 @@
+/*******************************************************************************************************	
+ 	Authors: Hanna Johansson and Katja Mäenpää.
+	File created: 2017-01-18
+
+	The file contains functions which handle the functionality of FindFoodFast startpage.
+
+	This file includes the functions:
+	- 
+
+/*******************************************************************************************************
+
+//
+
 
