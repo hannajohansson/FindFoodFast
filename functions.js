34c970b0d6fc5e84fdbc9a3964596de9a65ea9c7
Katja <katja.maenpaa@aalto.fi>
2017-01-23 16:19:10 +0100

    Add info hover and description

--- functions.js
+++ functions.js
@@ -135,30 +135,7 @@ function showMap(){
 		icon: 'http://www.google.com/mapfiles/markerD.png'
 	});
 
-
-
-
 	console.log("Successful to generate map.")
-
-
-
-
-
-}
-
-function lookup() {
-	var address = document.getElementById("address").value;
-	geocoder.geocode({'address': address},
-	function(results, status) {
-		if (status == google.maps.GeocoderStatus.OK) {
-			map. setCenter(results[0].geometry.location);
-			var marker = new google.maps.Marker( {
-				map: map, position: results[0].geometry.location});
-		}
-		else  {
-			alert("Geocode was not successful for the following reason: " + status);
-		}
-	});
 }
 
 
